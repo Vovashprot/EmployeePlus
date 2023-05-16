fd51906b73c82ca21ff06291e991071df4b7272e
Vovashprot <vova.protasenko@gmail.com>
2023-05-16 20:33:32 +0300

    1

--- List-master/demo/src/main/java/think/example/demo/Employee.java
+++ List-master/demo/src/main/java/think/example/demo/Employee.java
@@ -6,6 +6,8 @@ public class Employee {
     private String firstName;
     private String lastName;
 
+
+
     public Employee(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
